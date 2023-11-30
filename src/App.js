02c4db99e441ecf48b95f08fdd98a7f472930f74
Mohammad Zaid <zaid@Mohammads-MacBook-Air.local>
2023-11-30 23:40:11 +0530

    Finalise FIrst block

--- src/App.js
+++ src/App.js
@@ -7,56 +7,32 @@ function App() {
   const monthsmap = {
     Jan: 5, Feb: 2, Mar: 5, "Apr": 4, "May": 5, "Jun": 4, "Jul": 5, "Aug": 5, "Sept": 4, "Oct": 5, "Nov": 4, "Dec": 5
   }
-  const [months, setmonths] = useState("none");
+  const [months, setmonths] = useState("Jan");
 
   return (
     <div className="global">
       <div className="wrapper">
         <div className="wrapper2">
           {/* grid1 */}
-
+          <div className="grid1">
+            {[1, 2, 3, 4, 5, 6].map(e => {
+              return (
+                <div className="block1">
+                  <p className="para">{e}</p>
+                </div>
+              )
+            })}
+          </div>
 
           {/* grid2 */}
           <div className="grid2">
-            {[15, 16, 17, 18, 19, 20, -1,
-              13, 14, 0, 0, 0, 0, 21,
-              11, 12, 0, 0, 0, 0, 22,
-              9, 10, 0, 0, 0, 0, 23,
-              7, 8, 0, 0, 0, 0, 24,
-              5, 6, 0, 0, 0, 0, 25,
-              3, 4, 0, 0, 0, 0, 26,
-              1, 2, 31, 30, 29, 28, 27,
-            ].map(e => {
-              if (e == 0)
-                return (
-                  <div className="white">
-                    <p className="para"></p>
-                  </div>
-                )
-              else if (e == -1)
+            {[7, 8, 9, 10, 11, 12, 13, 14, 15, 16, , 50, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26].map(e => {
+              if (e == 50)
                 return (
                   <div className="deadblock">
                     <p className="para"></p>
                   </div>
                 )
-              else if (e > 27)
-                return (
-                  <div className="darkblock">
-                    <p className="para">{e}</p>
-                  </div>
-                )
-              else if (e == 27)
-                return (
-                  <div className="block27">
-                    <p className="para">{e}</p>
-                  </div>
-                )
-              else if (e > 28) {
-                if (months == "Feb")
-                  <div className="darkblock">
-                    <p className="para"></p>
-                  </div>
-              }
               else
                 return (
                   <div className="block1">
@@ -67,7 +43,31 @@ function App() {
           </div>
 
 
+          {/* third grid */}
+          <div className="grid1">
+            {[27, 28, 29, 30, 31, 32].map(e => {
+              let days = "";
+              for (const [key, value] of Object.entries(monthsmap)) {
+                if (months == key) {
+                  days = value;
+                }
+              }
+              if (e - 27 < days)
+                return (
+                  <div className="block3">
+                    <p className="para">{e}</p>
+                  </div>
+                )
+              else
+                return (
+                  <div className="deadblock">
+                    <p className="para"></p>
+                  </div>
+                )
 
+
+            })}
+          </div>
         </div>
 
       </div>
@@ -87,5 +87,4 @@ function App() {
     </div>
   );
 }
-
-export default App;
+export default App
